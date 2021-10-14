35a1b24709e416640ea4fbd97590456a8759f33f
fuyue <1945472730@qq.com>
2021-10-14 13:53:00 +0800

    chore: 修改基本模板

--- src/layouts/DragEditorLayout/index.tsx
+++ src/layouts/DragEditorLayout/index.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from 'react';
 
 const DragEditorLayout: FC = ({ children }) => {
-    console.log(123);
     return <div className="drag-editor-container">{children}</div>;
 };
 
